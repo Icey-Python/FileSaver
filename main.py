a7b19e26c04802d592e6ce4e88381227c1d1afac
Icey-Python <ndungusamkelly5@gmail.com>
2023-11-21 12:40:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import sqlite3
+import os
+import hashlib
+
+DB_FILE = 'myfiledb.db'
+
+def create_connection(db_file):
+    conn = sqlite3.connect(db_file)  
+    return conn
+
+def create_table(conn):
+    sql = '''CREATE TABLE IF NOT EXISTS files
+             (id INTEGER PRIMARY KEY, filename TEXT, filehash TEXT, file BLOB)'''
+    c = conn.cursor()
+    c.execute(sql)
+
+def insert_file(conn, file):
+    sql = 'INSERT INTO files (filename, filehash, file) VALUES (?,?,?)'  
+    c = conn.cursor()
+
+    with open(file, 'rb') as f:
+        data = f.read()
+        filehash = hashlib.md5(data).hexdigest()  
+        filename = os.path.basename(file)
+
+    c.execute(sql, (filename, filehash, data))
+    conn.commit() 
+
+def retrieve_file(conn, id):
+    sql = 'SELECT * FROM files WHERE id=?'
+    c = conn.cursor() 
+    c.execute(sql, (id,))
+    file_data = c.fetchone()
+
+    file_blob = file_data[3]
+    filename = file_data[1]
+
+    with open('output/' + filename, 'wb') as f: 
+        f.write(file_blob)
+
+def main():
+    conn = create_connection(DB_FILE)  
+    create_table(conn)
+
+    #simple file_saver storage  
+    file = 'WhatsApp Image 2023-11-21 at 09.44.37.jpeg' 
+
+    insert_file(conn, file) 
+
+    retrieve_file(conn, 1)
+
+    conn.close()
+
+if __name__ == '__main__':
+    main()
